53ed12016b2570d4699314c9653eba6d37b66996
Angel Javier Sanchez Tenjo <asanchez@xolit.com>
2023-06-14 00:55:38 -0500

    Initial htmlcss.py

--- pdf.py
+++ pdf.py
@@ -1,10 +1,11 @@
 import streamlit as st
 from dotenv import load_dotenv
-from html import bot_template, user_template, css
+from htmlcss import bot_template, user_template, css
 
 def main():
-	load_dotenv()
-	st.set_page_config(page_title="Chat con multiples PDFs", page_icon=":books:")
+    load_dotenv()
+    st.set_page_config(page_title="Chat con multiples PDFs", page_icon=":books:")
+    st.write(css, unsafe_allow_html=True)
 	
 if __name__ == '__main__':
     main()

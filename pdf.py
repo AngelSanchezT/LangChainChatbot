2f3549592310b5459434601647de313621b214cd
Angel Javier Sanchez Tenjo <asanchez@xolit.com>
2023-06-14 01:55:38 -0500

    Add Status Page

--- pdf.py
+++ pdf.py
@@ -6,6 +6,10 @@ def main():
     load_dotenv()
     st.set_page_config(page_title="Chat con multiples PDFs", page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
 	
 if __name__ == '__main__':
     main()

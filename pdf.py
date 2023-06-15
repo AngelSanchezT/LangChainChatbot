e3ca2a3d8f9193e4bfeb5747e9687316dba6e8bf
Angel Javier Sanchez Tenjo <asanchez@xolit.com>
2023-06-14 22:43:29 -0500

    Adition components and session states a our app

--- pdf.py
+++ pdf.py
@@ -10,6 +10,12 @@ def main():
         st.session_state.conversation = None
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
+    st.header("Chat con multiples PDFs :books:")
+    user_question = st.text_input("Pregúntale lo que quieras a tus documentos PDFs:")
+    with st.sidebar:
+        st.subheader("Tus documentos")
+        pdf_docs = st.file_uploader("Sube tus PDFs aquí y haz click en 'Procesar'", accept_multiple_files=True)
+
 	
 if __name__ == '__main__':
     main()

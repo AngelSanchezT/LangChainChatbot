74bcf86e5d9c5ff452116967db113db66787d6aa
Angel Javier Sanchez T <ajsanchez22@gmail.com>
2023-06-14 22:50:06 -0500

    Add method handle_userinput for handler the input the user

--- pdf.py
+++ pdf.py
@@ -27,6 +27,18 @@ def get_pdf_pages(pdf_docs):
         all_pages += pdf_pages
     return all_pages
 
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+
 def main():
     load_dotenv()
     st.set_page_config(page_title="Chat con multiples PDFs", page_icon=":books:")
